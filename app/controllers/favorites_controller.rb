bb1991a245b7e460b09a0d8aaba5292cd5b19090
Yuji YAMAMOTO <oo_hayjp@yahoo.co.jp>
2018-08-03 03:23:49 +0900

    他のユーザのお気に入り一覧画面で、削除の操作をした場合に発生するエラー回避の処理を追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,6 @@
 class FavoritesController < ApplicationController
-# before_action :logged_in_user, only: [:destroy]
+  before_action :logged_in_user, only: [:destroy]
+  before_action :set_favorite_user_id, only: [:destroy]
 
   def create
     favorite = current_user.favorites.create(radio_station_id: params[:id])
@@ -7,16 +8,20 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-  # if @radio_station.user_id.eql?(current_user.id)
-    favorite = current_user.favorites.find_by(radio_station_id: params[:id]).destroy
-    redirect_to radio_stations_path, notice: "Favorite radio station info was successfully destroyed."
-  # else
-  #   redirect_to root_path, notice: "Invalid User!"
-  # end
+    if @favorite_user_id.eql?(current_user.id)
+      favorite = current_user.favorites.find_by(radio_station_id: params[:id]).destroy
+      redirect_to radio_stations_path, notice: "Favorite radio station info was successfully destroyed."
+    else
+      redirect_to root_path, notice: "Invalid User!"
+    end
   end
 
-# private
-#   def logged_in_user
-#     redirect_to new_session_path unless logged_in?
-#   end
+  private
+    def logged_in_user
+      redirect_to new_session_path unless logged_in?
+    end
+
+    def set_favorite_user_id
+      @favorite_user_id = Favorite.find_by(radio_station_id: params[:id]).user_id
+    end
 end
