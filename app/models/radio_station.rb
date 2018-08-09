af5a9e83477b8a651d08e425651ce27e0da2dbc4
Yuji YAMAMOTO <oo_hayjp@yahoo.co.jp>
2018-08-09 14:10:34 +0900

    #6の適用

--- app/models/radio_station.rb
+++ app/models/radio_station.rb
@@ -1,10 +1,18 @@
 class RadioStation < ApplicationRecord
-  validates :callsign,  presence: true, length: { maximum: 7 },
-                    format: { with: /\A([A-Z]|[A-Z]{2})[A-Z]{1,3}[\-]?[A-Z0-9]{0,3}\z/ },
-                    uniqueness: { case_sensitive: :false }
-  validates :webcast_url, presence: true, length: { maximum: 255 },
-                    format: /\A#{URI::regexp(%w(http https))}\z/,
-                    uniqueness: { case_sensitive: :false }
+  validates(
+    :callsign,
+    presence: true,
+    length: { maximum: 7 },
+    format: { with: /\A([A-Z]|[A-Z]{2})[A-Z]{1,3}[\-]?[A-Z0-9]{0,3}\z/ },
+    uniqueness: { case_sensitive: :false }
+  )
+  validates(
+    :webcast_url,
+    presence: true,
+    length: { maximum: 255 },
+    format: /\A#{URI::regexp(%w(http https))}\z/,
+    uniqueness: { case_sensitive: :false }
+  )
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,22 @@
 class User < ApplicationRecord
-  validates :name,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-                    uniqueness: { case_sensitive: :false }
+  validates :name, presence: true, length: { maximum: 30 }
+  validates(
+    :email,
+    presence: true,
+    length: { maximum: 255 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+    uniqueness: { case_sensitive: :false }
+  )
   before_save { email.downcase! }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates(
+    :password,
+    presence: true,
+    length: { minimum: 6 },
+    allow_nil: true
+  )
   has_many :radio_stations
   has_many :favorites, -> { order(:id) }, dependent: :destroy
   has_many :favorite_stations, through: :favorites, source: :radio_station
-# mount_uploader :avatar, AvatarUploader
+  # mount_uploader :avatar, AvatarUploader
 end
