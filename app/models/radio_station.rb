8c42e0ac4e14a7ac866d865699a0feb79d792a6f
Yuji YAMAMOTO <oo_hayjp@yahoo.co.jp>
2018-08-09 14:30:57 +0900

    case_sensitiveオプションの記述を修正

--- app/models/radio_station.rb
+++ app/models/radio_station.rb
@@ -4,14 +4,14 @@ class RadioStation < ApplicationRecord
     presence: true,
     length: { maximum: 7 },
     format: { with: /\A([A-Z]|[A-Z]{2})[A-Z]{1,3}[\-]?[A-Z0-9]{0,3}\z/ },
-    uniqueness: { case_sensitive: :false }
+    uniqueness: { case_sensitive: false }
   )
   validates(
     :webcast_url,
     presence: true,
     length: { maximum: 255 },
     format: /\A#{URI::regexp(%w(http https))}\z/,
-    uniqueness: { case_sensitive: :false }
+    uniqueness: { case_sensitive: false }
   )
   belongs_to :user
   has_many :favorites, dependent: :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     presence: true,
     length: { maximum: 255 },
     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-    uniqueness: { case_sensitive: :false }
+    uniqueness: { case_sensitive: false }
   )
   before_save { email.downcase! }
   has_secure_password
