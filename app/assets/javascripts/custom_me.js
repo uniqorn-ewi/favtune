6eec20e17d3196b5ef83bb289dfd5ee46f675b55
Yuji YAMAMOTO <oo_hayjp@yahoo.co.jp>
2018-12-09 23:52:39 +0900

    ページ切り替え時、jQueryが実行されない処理を修正

--- app/assets/javascripts/custom_me.js
+++ app/assets/javascripts/custom_me.js
@@ -1,23 +1,26 @@
 $(function() {
-  'use strict';
-
   $(window).on('load', function() {
     $("#effect").prepend( '<div id="preloader"></div>');
     $("#preloader").fadeOut(2000);
   });
+});
 
-  $(window).scroll(function() {
-    if ($(this).scrollTop() < 50) {
-      $('#back-to-top').fadeOut();
-    } else {
-      $('#back-to-top').fadeIn();
-    }
-  });
+$(window).scroll(function() {
+  if ($(this).scrollTop() < 50) {
+    $('#back-to-top').fadeOut();
+  } else {
+    $('#back-to-top').fadeIn();
+  }
+});
 
-  $('#back-to-top').click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 1000, 'easeInOutExpo');
-    return false;
-  });
+// ページ切り替え時（初回ページも対象）
+$(document).on({
+  'turbolinks:load': function() {
+    $('#back-to-top').click(function() {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 1000, 'easeInOutExpo');
+      return false;
+    });
+  }
 });
