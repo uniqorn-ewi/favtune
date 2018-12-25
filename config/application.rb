e9cf4231b503b6259c9c947657dfef9a56596d74
Yuji YAMAMOTO <oo_hayjp@yahoo.co.jp>
2018-12-25 11:00:56 +0900

    lib配下のファイル(クラス)をロードする記述の追加

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Favtune
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.paths.add 'lib', eager_load: true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
